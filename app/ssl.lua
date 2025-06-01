3ee1d7960faaa31059ffc77c08b616938fff365b
cybercoder <vahid.lid@live.com>
2025-06-01 21:18:09 +0330

    replace ngx.var with ssl.servername

--- app/ssl.lua
+++ app/ssl.lua
@@ -1,7 +1,13 @@
 local ssl = require("ngx.ssl")
 
-local crt = ngx.shared.tls_crt_cache:get(ngx.var.host) or ngx.shared.tls_crt_cache:get("DEFAULT")
-local key = ngx.shared.tls_key_cache:get(ngx.var.host) or ngx.shared.tls_key_cache:get("DEFAULT")
+local server_name, err = ssl.server_name()
+if not server_name then
+    ngx.log(ngx.ERR, "failed to get SNI server name: ", err)
+    -- return ngx.exit(ngx.ERROR)
+end
+
+local crt = ngx.shared.tls_crt_cache:get(server_name) or ngx.shared.tls_crt_cache:get("DEFAULT")
+local key = ngx.shared.tls_key_cache:get(server_name) or ngx.shared.tls_key_cache:get("DEFAULT")
 
 ssl.clear_certs()
 ssl.set_cert(ssl.parse_pem_cert(crt))
