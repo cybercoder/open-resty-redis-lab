8ac4983d0fac8002bb3dbe0628a21694ecbd9468
cybercoder <vahid.lid@live.com>
2025-06-01 21:47:06 +0330

    Update ssl.lua

--- app/ssl.lua
+++ app/ssl.lua
@@ -5,9 +5,13 @@ if not server_name then
     ngx.log(ngx.ERR, "failed to get SNI server name: ", err)
     -- return ngx.exit(ngx.ERROR)
 end
+ngx.log(ngx.INFO, "SNI server name: ", server_name)
 
 local crt = ngx.shared.tls_crt_cache:get(server_name) or ngx.shared.tls_crt_cache:get("DEFAULT")
 local key = ngx.shared.tls_key_cache:get(server_name) or ngx.shared.tls_key_cache:get("DEFAULT")
+
+ngx.log(ngx.DEBUG, "crt: ", crt)
+ngx.log(ngx.DEBUG, "key: ", key)
 
 ssl.clear_certs()
 ssl.set_cert(ssl.parse_pem_cert(crt))
