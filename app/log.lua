d369fecb4578f52062039dc60b551591d85ac2eb
Vahid Alimohamadi <gitlab_admin_84c66b@example.com>
2026-01-01 12:42:01 +0330

    Update file log.lua

--- app/log.lua
+++ app/log.lua
@@ -9,10 +9,10 @@ metric_latency:observe(tonumber(ngx.var.request_time),
     { ngx.var.server_name, ngx.ctx.namespace or "default", ngx.ctx.cdn_gateway or "default" })
 -- Track cache status (HIT, MISS, BYPASS, etc.)
 
-metric_url_requests:inc(1,
-    { ngx.var.server_name, ngx.var.request_method, ngx.var.uri, cache_status, ngx.ctx.namespace or "default", ngx.ctx
-    .cdn_gateway or
-    "default" })
+-- metric_url_requests:inc(1,
+--     { ngx.var.server_name, ngx.var.request_method, ngx.var.uri, cache_status, ngx.ctx.namespace or "default", ngx.ctx
+--     .cdn_gateway or
+--     "default" })
 
 metric_cache_status:inc(1,
     { ngx.var.server_name, cache_status, ngx.ctx.namespace or "default", ngx.ctx.cdn_gateway or "default" })
